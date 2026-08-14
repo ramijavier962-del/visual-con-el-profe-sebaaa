e0e3ade7e6067c85b440b344a49284db3cda21b1
Ramiro Sebastian Tejada <ramijavier962@gmail.com>
2026-08-14 20:07:05 -0300

    java
    java

---
+++ profe seba-java/javaScrip.js
@@ -0,0 +1,44 @@
+//Tipos de datos y variables
+//variable asignada a un numero
+
+let numero = 10;
+let decimal = 3.14;
+let nombre = "Seba";
+let booleano = true;
+
+//Mostrar en consola los valores de las variables
+console.log(numero+" "+decimal+" "+nombre+" "+booleano);
+console.log("el número es:",numero);
+console.log("el decimal es:",decimal);
+console.log("el nombre es:",nombre);
+console.log("el booleano es:",booleano);
+
+//variable constante
+const PI = 3.1416;
+console.log("el valor de PI es:",PI);
+
+//operaciones aritmeticas
+let num1 = 10;
+let num2 = 20;
+let suma = num1 + num2;
+console.log("la suma es:",suma);
+console.log("la suma es:",num1 + num2);
+
+
+let resta = num1 - num2;
+console.log("la resta es:",resta);
+console.log("la resta es:",num1 - num2);
+
+
+let multiplicacion = num1 * num2;
+console.log("la multiplicación es:",multiplicacion);
+
+
+let division = num1 / num2;
+console.log("la división es:",division);   
+console.log("la división es:",num1 / num2); 
+
+
+
+
+
